717362beafb1e6a2ca398a3bd7a5afb8294213bc
tienthanh <tloven473@gmail.com>
2024-01-07 21:54:20 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Body from "./component/Body";
+import Footertop from "./component/Footertop";
+import Header from "./component/Header";
+
+const App =()=>{
+  return(
+    <>
+     <Header/>
+     <Body/>
+     <Footertop/>
+     <Footertop/>
+    </>
+  )
+}
+
+export default App;
